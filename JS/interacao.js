e17bc6bbc8c09b9af294cae2d84b25f70f625274
igorasr <igoraugustosouza00@gmail.com>
2021-11-17 22:08:01 -0300

    70%

---
+++ JS/interacao.js
@@ -0,0 +1,43 @@
+
+// ABRE E FECHA MENU RESPONSIVO
+
+const btnMenu = document.querySelector('#btn-menu');
+const body = document.querySelector('body');
+
+
+btnMenu.addEventListener("click", ()=>{
+    btnMenu.classList.contains('bi-list')
+    ? btnMenu.classList.replace('bi-list', 'bi-x')
+    : btnMenu.classList.replace('bi-x', 'bi-list')
+
+    body.classList.toggle('menu-nav-active');
+})
+
+
+
+
+/*ANIMACAO SCROLL*/
+
+const item = document.querySelectorAll("[data-animation]");
+
+
+const animeScroll = () => {
+
+    const windowTop = window.pageYOffset + window.innerHeight * 1;
+    
+    item.forEach(element => {
+        if (windowTop > element.offsetTop){
+            element.classList.add('animate');
+        }else {
+            element.classList.remove('animate')
+        }
+    });
+
+
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+}) ;
